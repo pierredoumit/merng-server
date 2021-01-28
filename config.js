b4dccf52fa18f86050e806f4c77b34b447d8a06b
Michel ALAKL <al.akl.michel@gmail.com>
2021-01-28 15:01:57 +0200

    deploy

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://user:6DFQUwzSZ5h1bK9T@cluster0.dy8jh.mongodb.net/React-GraphQL?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
+
